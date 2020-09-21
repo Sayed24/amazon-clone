46b0c0215af0a8c5412c4947fc5b79f2914533af
Sayed <sadatsayed92@gmail.com>
2020-09-20 18:49:20 -0700

    User Authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBuSZsAeP3TyYXfFh7OIbjy7HJ4B9TkmN0",
@@ -9,3 +11,10 @@ const firebaseConfig = {
   appId: "1:1096198226414:web:9e58c2f686c66649164eff",
   measurementId: "G-LPHGJHC8VT"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
